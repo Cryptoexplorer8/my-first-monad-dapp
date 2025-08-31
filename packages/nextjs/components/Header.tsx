3becf1c5a609ce85cd51155dc48ff86a60cac4dc
Cryptoexplorer8 <113311423+Cryptoexplorer8@users.noreply.github.com>
2025-08-31 15:37:59 +0000

    2nd commit

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -67,16 +67,6 @@ export const Header = () => {
   return (
     <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
       <div className="navbar-start w-auto lg:w-1/2">
-        <details className="dropdown" ref={burgerMenuRef}>
-          <ul
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow-sm bg-base-100 rounded-box w-52"
-            onClick={() => {
-              burgerMenuRef?.current?.removeAttribute("open");
-            }}
-          >
-            <HeaderMenuLinks />
-          </ul>
-        </details>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/Monad Logo.png" />
